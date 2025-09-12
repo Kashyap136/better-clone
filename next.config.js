4fc5abd2f8da321c3f16712abb338e56ae0847ea
Kashyap136 <kashyaphimanshu136@gmail.com>
2025-09-12 15:52:36 +0530

    Add proper Next.js configuration

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const config = {
+  eslint: {
+    ignoreDuringBuilds: true
+  },
+  compiler: {
+    styledComponents: true
+  }
+}
+
+export default config;
