fc0f473aaaeff85796b7d2c9191d11ca8d0a3d04
Kashyap136 <kashyaphimanshu136@gmail.com>
2025-09-12 15:49:59 +0530

    Update next.config.mjs to ignore ESLint during builds

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true
+  }
+}
+
+export default nextConfig@type {import('next').NextConfig} */
 const nextConfig = {};
 
 export default nextConfig;
